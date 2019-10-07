31c5ef101ee2e1d6e9cb353440eb0a8031b9c656
Tobi Adeleye <tobiadeleye@Oluwatobis-MacBook-Pro.local>
2019-10-07 14:58:10 -0400

    Setting up objects

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,48 @@ import { Link } from "gatsby"
 import styled, {keyframes} from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import '../components/global-styles'
 
 class IndexPage extends React.Component {
   render() {
   
     return (
   <Layout>
-    <SEO title="Home" />
     
-    <HelloWorld>Welcome to your new Gatsby site </HelloWorld>
-   
+    <SEO title="Home" />
+    <FirstHeadline>Moving Your Cargo
+Just Got Easier.</FirstHeadline>
+    <FirstParagraph>
+    Get An Instant Quotation And Enjoy 24/7 Customer Service. With Our Superior Execution, Kago Is Able To Ship Your Cargo So You Can Get More Done. 
+    </FirstParagraph>
+    
   </Layout>
   )
 }
 }
 
+
 export default IndexPage
 
+
+
+
 // STYLED CSS
-const ParagraphOne = styled.p`
-font-size:28px;
+const Paragraph = styled.p`
+font-size:18px;
 line-height: 25px;
-color: rgba(1,27,51,1.0)
+color: rgba(1,27,51,1.0);
+font-weight: 400;
+`
+const HeadlineOne = styled.h1`
+font-size:60px;
+line-height: 67px;
+color: rgba(0,6,69,1.0);
+font-weight: 500;
 `
 
-const HelloWorld = styled(ParagraphOne)`
+const FirstHeadline = styled(HeadlineOne)`
+`
+
+const FirstParagraph = styled(Paragraph)`
 
 `
