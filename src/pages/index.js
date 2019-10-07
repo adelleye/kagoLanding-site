d12375585ba638ccbf1047640972ffce2ed0237f
Tobi Adeleye <tobiadeleye@Oluwatobis-MacBook-Pro.local>
2019-10-07 13:26:13 -0400

    Added fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+class IndexPage extends React.Component {
+  render() {
+  
+    return (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    
+    <HelloWorld>Welcome to your new Gatsby site </HelloWorld>
+   
   </Layout>
-)
+  )
+}
+}
 
 export default IndexPage
+
+// STYLED CSS
+const ParagraphOne = styled.p`
+font-size:28px;
+line-height: 25px;
+color: rgba(1,27,51,1.0)
+`
+
+const HelloWorld = styled(ParagraphOne)`
+
+`
