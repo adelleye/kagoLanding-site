7b2ee5382d89e01135b76a9257d51a4f16e95ca9
Tobi Adeleye <tobiadeleye@Oluwatobis-MacBook-Pro.local>
2019-10-07 11:32:53 -0400

    installed styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
