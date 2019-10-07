30099ee4f68e9beb28d224b550abc535c96ee001
Tobi Adeleye <tobiadeleye@Oluwatobis-MacBook-Pro.local>
2019-10-07 19:07:47 -0400

    Header edit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,56 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  //When a component is created, the constructor will be called.
+  constructor(props) {
+    super(props);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    this.state = {
+      hasScrolled: false,
+    }
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  /*As soon as the component is loaded, we try to listen to the built-in scroll events passed from our window. This will in turn trigger the handleScroll function*/
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+  handleClick = () => {
+    window.open('http://kago-frontend-dev.herokuapp.com/')
+   }
+
+  /*The handleScroll function will get the Y position of the scrolling. When the scrolling hits more than 50px, it’ll set a new state for our hasScrolled boolean to true. Otherwise, it’ll set it back to false.*/
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div id="site-title">
+          <a>Kago</a>
+        </div>
+        <div className="HeaderGroup">
+          <Link to="/about">About</Link>
+          <Link to="/ourInstructors">Our Instructors</Link>
+          <Link to="/blog">Blog</Link>
+        </div>
+
+        <div id="get-started" >
+          <button onClick={this.handleClick}>Get Started</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,29 @@ import { Link } from "gatsby"
 import styled, {keyframes} from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import FirstIllustration from "../images/first-Illustration.png"
 
 class IndexPage extends React.Component {
   render() {
   
     return (
   <Layout>
-    
     <SEO title="Home" />
-    <FirstHeadline>Moving Your Cargo
-Just Got Easier.</FirstHeadline>
-    <FirstParagraph>
-    Get An Instant Quotation And Enjoy 24/7 Customer Service. With Our Superior Execution, Kago Is Able To Ship Your Cargo So You Can Get More Done. 
-    </FirstParagraph>
-    
+    <Container>
+      <LeftGrid>
+        <FirstHeadline>
+         Moving Your Cargo Just Got Easier.
+           </FirstHeadline>
+           <FirstParagraph>
+             Get An Instant Quotation And Enjoy 24/7 Customer Service. With Our Superior Execution, 
+           Kago Is Able To Ship Your Cargo So You Can Get More Done. 
+            </FirstParagraph>
+      </LeftGrid>
+      <RightGrid>
+      <TruckIllustration/>
+        
+      </RightGrid>
+    </Container>
   </Layout>
   )
 }
@@ -28,7 +37,7 @@ export default IndexPage
 
 
 
-// STYLED CSS
+// STYLED CSS--- Objects
 const Paragraph = styled.p`
 font-size:18px;
 line-height: 25px;
@@ -36,15 +45,35 @@ color: rgba(1,27,51,1.0);
 font-weight: 400;
 `
 const HeadlineOne = styled.h1`
-font-size:60px;
+font-size:55px;
 line-height: 67px;
 color: rgba(0,6,69,1.0);
 font-weight: 500;
 `
+// PAGE STYLING BEGINS HERE
+const Container = styled.div`
+display: grid;
+grid-template-columns: repeat(2,auto);
 
+`
+const LeftGrid = styled.div`
+margin-left: 80px;
+margin-top: 250px;
+max-width: 575px;
+`
+
+const RightGrid = styled.div`
+margin-top: 200px;
+`
 const FirstHeadline = styled(HeadlineOne)`
 `
 
 const FirstParagraph = styled(Paragraph)`
 
+`
+const TruckIllustration = styled.div`
+background: url(${FirstIllustration});
+background-size: cover;
+height: 395px;
+width: 526px;
 `
